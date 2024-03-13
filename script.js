45ea821d4bcfb8dd59e3a4a90d020a4287f40659
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-13 10:35:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(i=0;i<str.length;i++){
+    char = str[i];
+    if(str.indexOf(char)==i&&str.indexOf(char,i+1)==-1){
+      return char;
+    }
+  }
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
